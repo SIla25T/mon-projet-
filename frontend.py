c5576df5c81da2d23e0ae585bb20c27c9ff43701
TEsseiva <tt.esseiva@hotmail.com>
2026-04-21 11:37:27 +0200

    Bouton organisé

--- frontend.py
+++ frontend.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.title("Projet Python combinaison")
 
-if st.button("Texte 1"):
-    st.texte1()
-if st.button("Texte 2"):
-    st.texte2()
-if st.button("Combiner"):
-    st.combiner()
-if st.button("Exporter"):
-    st.exporter()
+left, middle, right = st.columns(3, vertical_alignment="bottom")
+
+left.button("Texte 1", use_container_width=True)
+right.button("Texte 2", use_container_width=True)
+
+left, middle, right = st.columns(3, vertical_alignment="bottom")
+middle.button("Combiner", use_container_width=True)
+middle.button("Exporter", use_container_width=True)
