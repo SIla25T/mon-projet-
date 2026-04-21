634e2724f652cfcf90c39d1f0ef7180f8bcd2ebc
Personne <agheza06@gmail.com>
2026-04-21 12:08:08 +0200

    21.04

---
+++ src/classe_abstraite.py
@@ -0,0 +1,10 @@
+"""Classe Conte"""
+
+from abc import ABC, abstractmethod
+
+class Conte(ABC):
+    """Classe abstraite représentant toutes les constituantes d'un conte.
+    toutes les sous-classes de Conte doivent avoir un id"""
+    @abstractmethod
+    def id(self):
+        """retourne l'id de la constituante du conte"""
