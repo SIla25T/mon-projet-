9fc7c81857eb8c5abc54d94b3b9bbd2844d390e2
TEsseiva <tt.esseiva@hotmail.com>
2026-04-21 11:26:49 +0200

    Apprendre streamlit
    Affichage des boutons nécessaire sur un frontend

---
+++ frontend.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Projet Python combinaison")
+
+if st.button("Texte 1"):
+    st.texte1()
+if st.button("Texte 2"):
+    st.texte2()
+if st.button("Combiner"):
+    st.combiner()
+if st.button("Exporter"):
+    st.exporter()
