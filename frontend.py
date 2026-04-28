e506c85d47c9230e721b4a81efd302cb253fb9aa
TEsseiva <tt.esseiva@hotmail.com>
2026-04-28 09:50:11 +0200

    Ajout de radio bouton

--- frontend.py
+++ frontend.py
@@ -8,5 +8,12 @@ left.button("Texte 1", use_container_width=True)
 right.button("Texte 2", use_container_width=True)
 
 left, middle, right = st.columns(3, vertical_alignment="bottom")
+
+middle.radio(
+    "Stratégie de fusion",
+    ["Union", "Intersection", "Priorité"]
+)
+
+left, middle, right = st.columns(3, vertical_alignment="bottom")
 middle.button("Combiner", use_container_width=True)
 middle.button("Exporter", use_container_width=True)
