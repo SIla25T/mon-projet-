8461bd4ac24ddac3e5e0ffa5e89a1c0599feaae9
guiguizzzz <armbrustervalentin7@gmail.com>
2026-05-12 12:57:52 +0200

    update sur inputs.py et frontend.py
    ajouter les fonctions d'importation des fichiers Json et ajouter des boutons download dans frontend.py

--- src/inputs.py
+++ src/inputs.py
@@ -2,6 +2,17 @@
 import json
 from pathlib import Path
 
+def import_json(uploaded_file):
+    """
+    Fonction d'importation de fichiers JSON.
+    """
+    if uploaded_file is None:
+        return None
+    try:
+        return json.load(uploaded_file)
+    except json.JSONDecodeError:
+        return None
+
 def input_text (path):
     """fonction d'extraction des données des fichiers jsons"""
     text = Path(path)
